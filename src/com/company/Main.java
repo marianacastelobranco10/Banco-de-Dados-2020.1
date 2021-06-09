ade237b82e01984c69563619997f2ad0f8506f53
marianacastelobranco10 <marianacastelobranco10@gmail.com>
2021-06-08 22:33:48 -0300

    Disciplina instanciada

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Aluno aluno = new Aluno();
+        Disciplina BD = new Disciplina();
         aluno.setName("Mariana");
         aluno.setId(1);
         aluno.nota1 = 3.5;
